0461981b093991cd64c017cebde3abe63eb40f43
chris <sincejuly16th@gmail.com>
2019-07-04 20:50:39 +0800

    init

---
+++ src/page/login.vue
@@ -0,0 +1,166 @@
+<template>
+  	<div class="login_page fillcontain">
+		<img :src="'/img/background.jpg'" class="image">
+	  	<transition name="form-fade" mode="in-out">
+	  		<section class="form_contianer" v-show="showLogin">
+		  		<div class="manage_tip">
+		  			<p>饱了么外卖管理系统</p>
+		  		</div>
+		    	<el-form :model="loginForm" :rules="rules" ref="loginForm">
+					<el-form-item prop="username">
+						<el-input v-model="loginForm.username" placeholder="用户名"><span>dsfsf</span></el-input>
+					</el-form-item>
+					<el-form-item prop="password">
+						<el-input type="password" placeholder="密码" v-model="loginForm.password"></el-input>
+					</el-form-item>
+					<el-form-item>
+				    	<el-button type="primary" @click="submitForm('loginForm')" class="submit_btn">登录</el-button>
+				  	</el-form-item>
+					<el-form-item>
+				    	<el-button type="primary" @click="submitForm('loginForm')" class="submit_btn">没有账号?立即注册</el-button>
+				  	</el-form-item>
+				</el-form>
+	  		</section>
+	  	</transition>
+  	</div>
+</template>
+
+<script>
+	import {login, getAdminInfo} from '@/api/getData'
+	import {mapActions, mapState} from 'vuex'
+	import {getStore, setStore} from '@/config/mUtils'
+	export default {
+	    data(){
+			return {
+				loginForm: {
+					username: '',
+					password: '',
+				},
+				rules: {
+					username: [
+			            { required: true, message: '请输入用户名', trigger: 'blur' },
+			        ],
+					password: [
+						{ required: true, message: '请输入密码', trigger: 'blur' }
+					],
+				},
+				showLogin: false,
+				localInfo: getStore("name")
+			}
+		},
+		mounted(){
+			this.showLogin = true;
+			// if (!this.adminInfo.id) {
+    		// 	this.getAdminData()
+			// }
+			this.localInfo = getStore("name")
+			if (this.localInfo) {
+				this.$message({
+                    type: 'success',
+                    message: '检测到您之前登录过，将自动登录'
+                });
+				this.$router.push('manage')
+			}
+		},
+		computed: {
+			...mapState(['adminInfo']),
+		},
+		methods: {
+			...mapActions(['getAdminData']),
+			async submitForm(formName) {
+				this.$refs[formName].validate(async (valid) => {
+					if (valid) {
+						const res = await login({user_name: this.loginForm.username, password: this.loginForm.password})
+						// try{
+						// 	const res = await getAdminInfo()
+						// 	this.adminInfo.admin = res.data.admin;
+						// 	this.adminInfo.avatar = res.data.avatar;
+						// 	this.adminInfo.city = res.data.city;
+						// 	this.adminInfo.create_time = res.data.create_time;
+						// 	this.adminInfo.id = res.data.id;
+						// 	this.adminInfo.user_name = res.data.user_name;
+						// 	if (res.status == 1) {
+						// 		alert('AdminInfo Get');
+						// 	}else{
+						// 		throw new Error(res.type)
+						// 	}
+						// }catch(err){
+						// 	console.log(err.message)
+						// }
+						setStore("name", this.loginForm.username)
+						if (res.status == 1) {
+							this.$message({
+		                        type: 'success',
+		                        message: '欢迎，' + this.loginForm.username + '!'
+		                    });
+							this.$router.push('manage')
+						}else{
+							this.$message({
+		                        type: 'error',
+		                        message: res.message
+		                    });
+						}
+					} else {
+						this.$notify.error({
+							title: '错误',
+							message: '请输入正确的用户名密码',
+							offset: 100
+						});
+						return false;
+					}
+				});
+			},
+		},
+		// watch: {
+		// 	adminInfo: function (newValue){
+		// 		if (newValue.id) {
+		// 			this.$message({
+        //                 type: 'success',
+        //                 message: '检测到您之前登录过，将自动登录'
+        //             });
+		// 			this.$router.push('manage')
+		// 		}
+		// 	}
+		// }
+	}
+</script>
+
+<style lang="less" scoped>
+	@import '../style/mixin';
+	.login_page{
+		background-color: #324057;
+	}
+	.manage_tip{
+		position: absolute;
+		width: 100%;
+		top: -100px;
+		left: 0;
+		p{
+			font-size: 34px;
+			color: #fff;
+		}
+	}
+	.form_contianer{
+		.wh(320px, 210px);
+		.ctp(320px, 210px);
+		padding: 25px;
+		border-radius: 5px;
+		text-align: center;
+		background-color: fade(#F5F5F5, 40%);
+		.submit_btn{
+			width: 100%;
+			font-size: 16px;
+		}
+	}
+	.tip{
+		font-size: 12px;
+		color: red;
+	}
+	.form-fade-enter-active, .form-fade-leave-active {
+	  	transition: all 1s;
+	}
+	.form-fade-enter, .form-fade-leave-active {
+	  	transform: translate3d(0, -50px, 0);
+	  	opacity: 0;
+	}
+</style>
