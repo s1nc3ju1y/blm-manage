8d86ba4eb5ebc7ab194413e16a39c74789f0f61f
chris <sincejuly16th@gmail.com>
2019-07-06 17:18:44 +0800

    能跑了

--- src/config/env.js
+++ src/config/env.js
@@ -14,8 +14,8 @@ if (process.env.NODE_ENV == 'development') {
 	baseUrl = '';
     baseImgPath = '/img/';
 }else{
-	baseUrl = '//elm.cangdu.org';
-    baseImgPath = '//elm.cangdu.org/img/';
+	baseUrl = 'http://localhost:8001';
+    baseImgPath = 'http://localhost:8001/img/';
 }
 
 export {
